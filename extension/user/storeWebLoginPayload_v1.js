e10f83d4c8e6eacce02d36e0e5e592a37655c1fb
rutkowski <waldemar.rutkowski@shopgate.com>
2018-01-11 17:23:10 +0100

    WEBC-541: Added two helper functions to load and save the payload of a web-login initialization

--- extension/user/storeWebLoginPayload_v1.js
+++ extension/user/storeWebLoginPayload_v1.js
@@ -9,6 +9,32 @@
  */
 module.exports = function (context, input, cb) {
   context.log.info('Storing payload: ' + JSON.stringify(input.payload))
+  saveWebLoginPayload(intput.payload, context, cb)
+}
 
-  return cb(null, {})
+// /**
+//  * Loads the web login payload if there is anything
+//  *
+//  * @param {Object} context
+//  * @param {function({Object}, {string})} cb
+//  */
+// function loadWebLoginPayload (context, cb) {
+//   // use device storage only
+//   context.storage.device.get('webLoginPayload', (err, payload) => {
+//     return cb(err, payload)
+//   })
+// }
+
+/**
+ * Saves the given web login payload into internal storage (user or device)
+ *
+ * @param {string} payload
+ * @param {Object} context
+ * @param {function({Object})} cb
+ */
+function saveWebLoginPayload (payload, context, cb) {
+  // use device storage only
+  context.storage.device.set('webLoginPayload', payload, (err) => {
+    return cb(err || null)
+  })
 }
