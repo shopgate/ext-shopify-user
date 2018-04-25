0e11f219f2fbefe24fcb6fdce878e88a5a914601
Ronny Lindner <ronny.lindner@shopgate.com>
2018-04-25 17:22:43 +0200

    WEBC-671 - Added a short sleep to prevent errors while register-login-checkout-process

--- extension/user/requestShopifyUserId_v1.js
+++ extension/user/requestShopifyUserId_v1.js
@@ -1,5 +1,6 @@
 const Shopify = require('../lib/shopify.api.js')
 const CustomerNotFoundError = require('../models/Errors/CustomerNotFoundError')
+const Sleep = require('sleep')
 
 /**
  * @typedef {Object} ShopifyCustomerAccessToken
@@ -22,6 +23,11 @@ const CustomerNotFoundError = require('../models/Errors/CustomerNotFoundError')
 module.exports = function (context, input, cb) {
   const shopify = Shopify(context.config)
 
+  /*
+   * In some cases Shopify is not fast enough after registration. Therefor we have to wait a moment here.
+   * Especially if the user registers within the checkout process.
+   */
+  Sleep.msleep(1000)
   shopify.findUserByEmail(input.login, (err, customerList) => {
     /**
      * Ensure the requested data to be available and no request error occurred.
