2fdd2b1ff53bcd62f7a119c733de6627d59cfa9f
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-04-26 15:28:11 +0200

    PWA-152 Subscribe register and checkout links to open outer pages

---
+++ frontend/Register/subscribers.js
@@ -0,0 +1,28 @@
+import fetchRegisterUrl from '@shopgate/pwa-common/actions/user/fetchRegisterUrl'
+import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory'
+import { getRegisterUrl } from '@shopgate/pwa-common/selectors/user'
+import ParsedLink from '@shopgate/pwa-common/components/Router/helpers/parsed-link'
+import {openedRegisterLink$} from '@shopgate/pwa-common/streams/history'
+import openRegisterUrl from '@shopgate/pwa-common/subscriptions/helpers/openRegisterUrl'
+import { LEGACY_URL } from '@shopgate/pwa-common/constants/Registration'
+
+export default (subscribe) => {
+  // Open register link subscription
+  subscribe(openedRegisterLink$, async ({ dispatch, getState }) => {
+    const state = getState()
+
+    const hasRegistrationUrl = !!getRegisterUrl(state)
+
+    // Open the registration url if one is found.
+    if (hasRegistrationUrl) {
+      await dispatch(fetchRegisterUrl())
+        .then(url => openRegisterUrl(url, state))
+        .catch(e => e)
+    } else {
+      const link = new ParsedLink(LEGACY_URL)
+      link.open()
+    }
+
+    dispatch(goBackHistory(1))
+  })
+}
