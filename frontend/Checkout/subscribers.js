c3e10a7d6579badc33fcc3a2ee70d3ae61b13237
devbucket <florian.mueller81@gmx.de>
2018-05-08 17:26:01 +0200

    PWA-152 Moved frontend code to eslint and shopgate-eslint-config.

--- frontend/Checkout/subscribers.js
+++ frontend/Checkout/subscribers.js
@@ -1,23 +1,23 @@
-import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory'
-import { isUserLoggedIn } from '@shopgate/pwa-common/selectors/user'
-import ParsedLink from '@shopgate/pwa-common/components/Router/helpers/parsed-link'
-import trackingCore from '@shopgate/tracking-core/core/Core'
-import { LEGACY_URL } from '@shopgate/pwa-common-commerce/checkout/constants'
-import { openedCheckoutLink$ } from '@shopgate/pwa-common-commerce/checkout/streams'
-import fetchCheckoutUrl from '@shopgate/pwa-common-commerce/checkout/actions/fetchCheckoutUrl'
+import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
+import { isUserLoggedIn } from '@shopgate/pwa-common/selectors/user';
+import ParsedLink from '@shopgate/pwa-common/components/Router/helpers/parsed-link';
+import trackingCore from '@shopgate/tracking-core/core/Core';
+import { LEGACY_URL } from '@shopgate/pwa-common-commerce/checkout/constants';
+import { openedCheckoutLink$ } from '@shopgate/pwa-common-commerce/checkout/streams';
+import fetchCheckoutUrl from '@shopgate/pwa-common-commerce/checkout/actions/fetchCheckoutUrl';
 
 /**
  * Checkout subscriptions.
  * @param {Function} subscribe The subscribe function.
  */
-export default function checkout (subscribe) {
+export default function checkout(subscribe) {
   /**
    * Gets triggered when the user enters the checkout.
    */
   subscribe(openedCheckoutLink$, ({ dispatch, getState }) => {
     // Check if user is logged in.
     if (!isUserLoggedIn(getState())) {
-      return
+      return;
     }
 
     dispatch(fetchCheckoutUrl())
@@ -27,14 +27,14 @@ export default function checkout (subscribe) {
          * legacy url if the Pipeline returns an invalid url.
          * Add some tracking params for cross domain tracking.
          */
-        const checkoutUrl = trackingCore.crossDomainTracking(url || LEGACY_URL)
+        const checkoutUrl = trackingCore.crossDomainTracking(url || LEGACY_URL);
 
         // Open the checkout.
-        const link = new ParsedLink(checkoutUrl)
-        link.open()
+        const link = new ParsedLink(checkoutUrl);
+        link.open();
       })
-      .catch(e => e)
+      .catch(e => e);
 
-    dispatch(goBackHistory(1))
-  })
+    dispatch(goBackHistory(1));
+  });
 }
