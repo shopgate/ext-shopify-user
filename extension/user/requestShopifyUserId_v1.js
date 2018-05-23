56cd586449687e472278182623a1a4fdb6761702
Ronny Lindner <ronny.lindner@shopgate.com>
2018-05-23 17:14:46 +0200

    temp commit number 2, routing is still broken

--- extension/user/requestShopifyUserId_v1.js
+++ extension/user/requestShopifyUserId_v1.js
@@ -2,8 +2,13 @@ const Shopify = require('../lib/shopify.api.js')
 const CustomerNotFoundError = require('../models/Errors/CustomerNotFoundError')
 const Sleep = require('sleep')
 
-function canFindUserByEmail (shopify, input) {
-  shopify.findUserByEmail(input.login, (err, customerList) => {
+function findUserByEmail(email, shopify) {
+  shopify.findUserByEmail(email, (err, customerList) => {
+
+    console.log('+++  CustomerList:')
+    console.log(customerList)
+    console.log('+++  +++ +++ +++ +++')
+
     /**
      * Ensure the requested data to be available and no request error occurred.
      *
@@ -17,6 +22,7 @@ function canFindUserByEmail (shopify, input) {
     return customerList[0].id.toString()
   })
 }
+
 /**
  * @typedef {Object} ShopifyCustomerAccessToken
  * @property {string} accessToken
@@ -42,26 +48,44 @@ module.exports = function (context, input, cb) {
   console.log('Strategy: ' + input.strategy)
   console.log('++++++++++++++')
 
-  /*
-   * In some cases Shopify is not fast enough after registration. Therefor we have to wait a moment here.
-   * Especially if the user registers within the checkout process. In this case we try multiple times to fetch the
-   * userId. If we're not able, the step will return a CustomerNotFoundError
-   */
-  const maxTryCount = 6
+  // Default login via login form
+  if (input.strategy === 'basic') {
+    shopify.findUserByEmail(input.login, (err, customerList) => {
+      /**
+       * Ensure the requested data to be available and no request error occurred.
+       *
+       * @typedef {Object} CustomerResponseElement
+       * @property {number} id
+       */
+      if (err || !customerList || customerList.length < 1) {
+        return cb(new CustomerNotFoundError())
+      }
 
-  for (let tryCount = 1; tryCount <= maxTryCount; tryCount++) {
-    console.log(Date.now() + '##### - tryCount: ' + tryCount)
-    const userId = canFindUserByEmail(shopify, input)
+      const filterResult = (customerList.filter((customer) => {
+        return customer.email === input.login.toString()
+      }))
 
-    if (userId) {
-      console.log(Date.now() + '##### - userId found: ' + userId)
-      return cb(null, {userId})
-    }
-
-    console.log(Date.now() + '##### - userId not found, going to sleep and try again')
-    Sleep.msleep(2000)
+      return filterResult.length
+        ? cb(null, {'userId': filterResult[0].id.toString()})
+        : cb(new CustomerNotFoundError())
+    })
   }
 
-  // TODO implement context.log here
+  // Forced login after customer has registered, for example within the checkout process
+  console.log('###  Forced login  ###')
+  const maxTries = 5
+  const sleepDelay = 2000
+
+  for (let tryCount = 1; tryCount <= maxTries; tryCount++) {
+    console.log('### - Login tryCount: ' + tryCount)
+    const userId = findUserByEmail(input.login, shopify)
+    if (userId) {
+      console.log('!!!!!!!! user found')
+      return cb(null, {userId})
+    }
+    Sleep.msleep(sleepDelay)
+    console.log('###########################')
+  }
+
   cb(new CustomerNotFoundError())
 }
