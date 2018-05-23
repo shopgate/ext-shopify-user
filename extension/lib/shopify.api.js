bad35633fe76f87238941984cbc3e661d96b9ea3
Ronny Lindner <ronny.lindner@shopgate.com>
2018-05-23 15:13:35 +0200

    temp commit, something seems to be broken

--- extension/lib/shopify.api.js
+++ extension/lib/shopify.api.js
@@ -158,7 +158,8 @@ module.exports = function (config) {
    * @param {function} cb
    */
   module.findUserByEmail = function (email, cb) {
-    this.get(`/admin/customers/search.json?query=${email}`, {}, (err, userData) => {
+    this.get(`/admin/customers/search.json?query=email:${email}&fields=id,email&limit=1`, {}, (err, userData) => {
+
       if (err) {
         return cb(err)
       }
--- extension/user/requestShopifyUserId_v1.js
+++ extension/user/requestShopifyUserId_v1.js
@@ -2,6 +2,21 @@ const Shopify = require('../lib/shopify.api.js')
 const CustomerNotFoundError = require('../models/Errors/CustomerNotFoundError')
 const Sleep = require('sleep')
 
+function canFindUserByEmail (shopify, input) {
+  shopify.findUserByEmail(input.login, (err, customerList) => {
+    /**
+     * Ensure the requested data to be available and no request error occurred.
+     *
+     * @typedef {Object} CustomerResponseElement
+     * @property {number} id
+     */
+    if (err || !customerList || customerList.length < 1) {
+      return false
+    }
+
+    return customerList[0].id.toString()
+  })
+}
 /**
  * @typedef {Object} ShopifyCustomerAccessToken
  * @property {string} accessToken
@@ -23,23 +38,30 @@ const Sleep = require('sleep')
 module.exports = function (context, input, cb) {
   const shopify = Shopify(context.config)
 
+  console.log('++++++++++++++')
+  console.log('Strategy: ' + input.strategy)
+  console.log('++++++++++++++')
+
   /*
    * In some cases Shopify is not fast enough after registration. Therefor we have to wait a moment here.
-   * Especially if the user registers within the checkout process.
+   * Especially if the user registers within the checkout process. In this case we try multiple times to fetch the
+   * userId. If we're not able, the step will return a CustomerNotFoundError
    */
-  Sleep.msleep(1000)
-  shopify.findUserByEmail(input.login, (err, customerList) => {
-    /**
-     * Ensure the requested data to be available and no request error occurred.
-     *
-     * @typedef {Object} CustomerResponseElement
-     * @property {number} id
-     */
-    if (err || !customerList || customerList.length < 1) {
-      return cb(new CustomerNotFoundError())
+  const maxTryCount = 6
+
+  for (let tryCount = 1; tryCount <= maxTryCount; tryCount++) {
+    console.log(Date.now() + '##### - tryCount: ' + tryCount)
+    const userId = canFindUserByEmail(shopify, input)
+
+    if (userId) {
+      console.log(Date.now() + '##### - userId found: ' + userId)
+      return cb(null, {userId})
     }
 
-    const userId = customerList[0].id.toString()
-    cb(null, {userId})
-  })
+    console.log(Date.now() + '##### - userId not found, going to sleep and try again')
+    Sleep.msleep(2000)
+  }
+
+  // TODO implement context.log here
+  cb(new CustomerNotFoundError())
 }
