3a148a61b7d72ca0a7e456d4517367ba5bef5740
Ronny Lindner <ronny.lindner@shopgate.com>
2018-07-27 08:31:02 +0200

    SHOPIFY-457 - PR-Rework

--- extension/user/addAddress_v1.js
+++ extension/user/addAddress_v1.js
@@ -11,14 +11,12 @@ const SGShopifyApi = require('../lib/shopify.api.class.js')
 module.exports = async function (context, input, cb) {
   // Check if there is a userId within the context.meta-data, if not the user is not logged
   if (Tools.isEmpty(context.meta.userId)) {
-    console.log('########## Not logged in ###############')
     return cb(new UnauthorizedError('User is not logged in.'))
   }
 
   const userId = context.meta.userId
 
   if (Tools.isEmpty(input.address)) {
-    console.log('########## Invalid call ###############')
     return cb(InvalidCallError('No or empty address data.'))
   }
 
@@ -33,17 +31,14 @@ module.exports = async function (context, input, cb) {
       first_name: address.firstName,
       last_name: address.lastName,
       phone: address.phone,
-      province: null, // This field is not in use at the moment, but could be important later (e.g. for us shops)
+      province: address.province,
       country: address.country,
-      zip: address.zipcode,
+      zip: address.zipCode,
       name: address.firstName + ' ' + address.lastName, // Not in use yet in the frontend
-      province_code: null, // This field is not in use at the moment, but could be important later (e.g. for us shops)
-      country_code: address.state,
-      country_name: address.country // From Shopify API-Doc "The customer’s normalized country name.", not sure if we need this
+      tags: []
     }
   }
 
-  // TODO create new endpoint in shopify api class to post the data to the admin endpoint
   const shopify = new SGShopifyApi(context)
 
   return shopify.addAddress(userId, newAddress)
