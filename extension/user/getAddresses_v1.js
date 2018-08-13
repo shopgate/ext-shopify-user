254ab1ee10fb663952e11c76f1eee48c8f9d1d1b
Konstantin Kiritsenko <k@componentk.com>
2018-08-13 17:05:26 +0300

    SHOPIFY-489 Removed the need to resolve the Promise

--- extension/user/getAddresses_v1.js
+++ extension/user/getAddresses_v1.js
@@ -11,33 +11,32 @@ module.exports = async function (context) {
     return new UnauthorizedError('Unauthorized user')
   }
 
-  return new SGShopifyApi(context).getAddresses(context.meta.userId).then(shopifyAddresses => {
-    const addresses = []
-    shopifyAddresses.forEach(address => {
-      const customerAddress = {
-        id: address.id,
-        street1: address.address1,
-        street2: address.address2,
-        city: address.city,
-        company: address.company,
-        firstName: address.first_name,
-        lastName: address.last_name,
-        phone: address.phone,
-        province: address.province,
-        province_code: address.province_code,
-        zipCode: address.zip,
-        country: address.country,
-        country_code: address.country_code,
-        tags: []
-      }
+  const shopifyAddresses = await new SGShopifyApi(context).getAddresses(context.meta.userId)
+  const addresses = []
+  shopifyAddresses.forEach(address => {
+    const customerAddress = {
+      id: address.id,
+      street1: address.address1,
+      street2: address.address2,
+      city: address.city,
+      company: address.company,
+      firstName: address.first_name,
+      lastName: address.last_name,
+      phone: address.phone,
+      province: address.province,
+      province_code: address.province_code,
+      zipCode: address.zip,
+      country: address.country,
+      country_code: address.country_code,
+      tags: []
+    }
 
-      if (address.default === true) {
-        customerAddress.tags.push('default')
-      }
+    if (address.default === true) {
+      customerAddress.tags.push('default')
+    }
 
-      addresses.push(customerAddress)
-    })
-
-    return {addresses}
+    addresses.push(customerAddress)
   })
+
+  return {addresses}
 }
