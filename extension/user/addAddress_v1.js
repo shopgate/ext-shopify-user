8b76ad6e91df330e3a3d1461636728e9f08f00a7
Konstantin Kiritsenko <k@componentk.com>
2018-08-13 21:16:07 +0300

    SHOPIFY-447 addAddress cleanup
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- extension/user/addAddress_v1.js
+++ extension/user/addAddress_v1.js
@@ -2,28 +2,24 @@ const Tools = require('../lib/tools')
 const UnauthorizedError = require('../models/Errors/UnauthorizedError')
 const InvalidCallError = require('../models/Errors/InvalidCallError')
 const SGShopifyApi = require('../lib/shopify.api.class')
+const {mapCountry} = require('../lib/mapper')
 
 /**
  * @param {SDKContext} context
  * @param {Object} input
- * @param {function} cb
  */
-module.exports = async function (context, input, cb) {
-  // Check if there is a userId within the context.meta-data, if not the user is not logged
+module.exports = async function (context, input) {
   if (Tools.isEmpty(context.meta.userId)) {
-    context.log.error('User is not logged in')
-    return cb(new UnauthorizedError('User is not logged in.'))
+    throw new UnauthorizedError('User is not logged in.')
   }
 
-  const userId = context.meta.userId
-
   if (Tools.isEmpty(input.address)) {
-    context.log.error('Empty address data.')
-    return cb(new InvalidCallError('Empty address data.'))
+    throw new InvalidCallError('Empty address data.')
   }
 
   // Map the input address values to fit the Shopify specifications for the admin api endpoint
   const address = Object.assign(input.address)
+  /** @var {ShopifyAddress} newAddress */
   const newAddress = {
     address1: address.street1,
     address2: address.street2,
@@ -35,10 +31,8 @@ module.exports = async function (context, input, cb) {
     province_code: address.province,
     zip: address.zipCode,
     name: address.firstName + ' ' + address.lastName,
-    ...SGShopifyApi.mapCountry(address.country)
+    ...mapCountry(address.country)
   }
 
-  const shopify = new SGShopifyApi(context)
-
-  return shopify.addAddress(userId, newAddress)
+  return new SGShopifyApi(context).addAddress(context.meta.userId, newAddress)
 }
