e7b113dd2ab8fc8d78636f09010de59879c5f4ea
Ronny Lindner <ronny.lindner@shopgate.com>
2018-09-05 14:27:15 +0200

    SHOPIFY-506 - removed console.log, added typedef

--- extension/user/setDefaultAddress_v1.js
+++ extension/user/setDefaultAddress_v1.js
@@ -3,6 +3,9 @@ const UnauthorizedError = require('../models/Errors/UnauthorizedError')
 const SGShopifyApi = require('../lib/shopify.api.class')
 
 /**
+ * @typedef {ShopgateAddress} input
+ * @property {Array} tags
+ *
  * @param {SDKContext} context
  * @param {ShopgateAddress} input
  */
@@ -12,9 +15,6 @@ module.exports = async function (context, input) {
   }
 
   if (!Tools.isEmpty(input.tags) && input.tags.includes('default')) {
-    console.log('##############')
-    console.log('UPDATING')
-    console.log('##############')
     return new SGShopifyApi(context).setDefaultAddress(context.meta.userId, input.id)
   }
 
