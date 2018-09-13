b42b126ddff3e2de0e9049db82f8e9d438a97847
Timo Ebel <timo.ebel@gmail.com>
2018-09-13 17:12:33 +0200

    PI-8958 Added saving customer tokens by ID to the extension storage as well.

--- extension/user/saveCustomerAccessToken_v1.js
+++ extension/user/saveCustomerAccessToken_v1.js
@@ -1,3 +1,12 @@
+/**
+ * @param {SDKContext} context
+ * @param {Object} input
+ * @param {string} input.customerAccessToken
+ * @param {string} input.userId
+ * @returns {Promise<void>}
+ * @throws Error when saving the customer access token fails.
+ */
 module.exports = async function (context, input) {
   await context.storage.user.set('customerAccessToken', input.customerAccessToken)
+  await context.storage.extension.map.setItem('customerTokensByUserIds', input.userId, input.customerAccessToken)
 }
