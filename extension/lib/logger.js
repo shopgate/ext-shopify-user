a2e16c773b0e166040dff03816217c1a37e256b5
Timo Ebel <timo.ebel@gmail.com>
2018-10-11 13:48:30 +0200

    SHOPIFY-546 Renamed .response.body to .response.body1 to see if it is really all about that property name.

--- extension/lib/logger.js
+++ extension/lib/logger.js
@@ -25,7 +25,7 @@ module.exports = class {
       /* request: requestOptions, */
       response: {
         headers: logResponse.headers || {},
-        body: 'dummy test value'
+        body1: 'dummy test value'
       },
       msg: 'Request to Shopify - User extension'
     })
