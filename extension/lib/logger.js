afc1ce19060f3286f4892ab2499dc4bab4bbdf56
Timo Ebel <timo.ebel@gmail.com>
2018-10-11 14:02:18 +0200

    SHOPIFY-546 Property name change.

--- extension/lib/logger.js
+++ extension/lib/logger.js
@@ -25,7 +25,7 @@ module.exports = class {
       /* request: requestOptions, */
       response: {
         headers: logResponse.headers || {},
-        body1: 'dummy test value'
+        somethingTotallyDifferent: 'dummy test value'
       },
       msg: 'Request to Shopify - User extension'
     })
