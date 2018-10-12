c8c3addad7d266e1104c002c90cba1fa8cc51db3
Timo Ebel <timo.ebel@gmail.com>
2018-10-12 16:41:09 +0200

    SHOPIFY-571 Changed parameter/properties check according to PR feedback.

--- extension/lib/mapper.js
+++ extension/lib/mapper.js
@@ -5,16 +5,13 @@
  * @returns {Object}
  */
 module.exports.mapCountry = function (country) {
-  if (!country || !country.length || country.length < 2) {
-    return {}
-  }
-
   const map = {}
-  if (country.length === 2) {
+  if (country && country.length && country.length === 2) {
     map.country_code = country
-  } else if (country.length > 2) {
+  } else if (country && country.length > 2) {
     map.country = country
   }
+
   return map
 }
 
@@ -23,14 +20,10 @@ module.exports.mapCountry = function (country) {
  * @return {Object}
  */
 module.exports.mapProvince = function (province) {
-  if (!province || !province.length || province.length < 2) {
-    return {}
-  }
-
   const map = {}
-  if (province.length === 2) {
+  if (province && province.length && province.length === 2) {
     map.province_code = province
-  } else if (province.length > 2) {
+  } else if (province && province.length > 2) {
     map.province = province
   }
 
