43afdd84715e31c9f49efce0088f67d7799611f1
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-11-05 11:15:05 +0100

    PWA-776 Add default config for shopify shops

--- extension/config/getConfig.js
+++ extension/config/getConfig.js
@@ -2,5 +2,87 @@
  * @return {Promise<Object>}
  */
 module.exports = async () => ({
-  splitDefaultAddressesByTags: ['default']
+  addressDefaultGroups: ['default'],
+  userMenuEntries: {
+    addressBook: true,
+    accountProfile: false
+  },
+  addressForm: {
+    fields: {
+      city: {
+        type: 'text',
+        label: 'City',
+        sortOrder: 6
+      },
+      custom: {
+        phone: {
+          type: 'phone',
+          label: 'Phone',
+          sortOrder: 10
+        },
+        company: {
+          type: 'text',
+          label: 'Company',
+          sortOrder: 3
+        }
+      },
+      country: {
+        type: 'country',
+        label: 'Country',
+        countries: [
+          'DE',
+          'US',
+          'AT',
+          'FR',
+          'GB'
+        ],
+        sortOrder: 7
+      },
+      street1: {
+        type: 'text',
+        label: 'Address1',
+        sortOrder: 4
+      },
+      street2: {
+        type: 'text',
+        label: 'Address2',
+        sortOrder: 5
+      },
+      zipCode: {
+        type: 'text',
+        label: 'Postal/Zip Code',
+        sortOrder: 9
+      },
+      lastName: {
+        type: 'text',
+        label: 'Last Name',
+        sortOrder: 2
+      },
+      province: {
+        type: 'province',
+        label: 'Province',
+        actions: [
+          {
+            type: 'setVisibility',
+            rules: [
+              {
+                data: [
+                  'US'
+                ],
+                type: 'oneOf',
+                context: 'country'
+              }
+            ]
+          }
+        ],
+        required: true,
+        sortOrder: 8
+      },
+      firstName: {
+        type: 'text',
+        label: 'First Name',
+        sortOrder: 1
+      }
+    }
+  }
 })
