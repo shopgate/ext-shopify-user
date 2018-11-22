9a8f038954b6629e661dee5b6ef95976d1569076
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-11-22 16:24:08 +0100

    PWA-1210 Fix subscription to go to register

--- frontend/Register/subscribers.js
+++ frontend/Register/subscribers.js
@@ -1,4 +1,3 @@
-import get from 'lodash/get';
 import { appWillStart$ } from '@shopgate/pwa-common/streams/app';
 import { redirects } from '@shopgate/pwa-common/collections';
 import { LEGACY_URL } from '@shopgate/pwa-common/constants/Registration';
@@ -6,24 +5,23 @@ import { REGISTER_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
 import buildRegisterUrl from '@shopgate/pwa-common/subscriptions/helpers/buildRegisterUrl';
 import fetchRegisterUrl from '@shopgate/pwa-common/actions/user/fetchRegisterUrl';
 import { getRegisterUrl } from '@shopgate/pwa-common/selectors/user';
+import { getCurrentRoute } from '@shopgate/pwa-common/helpers/router';
 
 export default (subscribe) => {
   /**
    * @param {Object} params The handler parameters.
    * @param {Function} params.dispatch The Redux dispatch function.
    * @param {Function} params.getState The Redux getState function.
-   * @param {Object} params.action Redux action object from the NAVIGATE action.
    * @return {Promise<string>}
    */
-  const redirectHandler = async ({ dispatch, getState, action }) => {
+  const redirectHandler = async ({ dispatch, getState }) => {
     /**
      * When the register url was opened from a login page, a redirect to the original target
      * page needs to happen after a successful registration. It's added by buildRegisterUrl.
      */
-    const location = get(action, 'params.state.redirect.location', '');
+    const { state: {redirect: {location = ''} = {}} } = getCurrentRoute();
 
     let url = getRegisterUrl(getState());
-
     if (!url) {
       // Fetch a fresh url if none was found within the store.
       url = await dispatch(fetchRegisterUrl());
