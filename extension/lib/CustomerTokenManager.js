a83106c4f098a62832bef932e3a3ef9873810a4d
Pascal Vomhoff <Pascal.Vomhoff@shopgate.com>
2019-05-01 01:14:04 +0200

    Hotfix if renewed token is removed

--- extension/lib/CustomerTokenManager.js
+++ extension/lib/CustomerTokenManager.js
@@ -28,7 +28,7 @@ module.exports = class {
     if (customerAccessToken.expiresAt) {
       try {
         const renewedToken = await this.extensionStorage.map.getItem('customerTokensByUserIds', this.userId)
-        if (Date.parse(renewedToken.expiresAt) > Date.parse(customerAccessToken.expiresAt)) {
+        if (renewedToken && Date.parse(renewedToken.expiresAt) > Date.parse(customerAccessToken.expiresAt)) {
           customerAccessToken = renewedToken
           await this.userStorage.set('customerAccessToken', renewedToken)
         }
