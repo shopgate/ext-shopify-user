0e2498f54465aa69b1df1b66ba79d03097834811
Alexander Wesselburg <info@myportal.de>
2019-05-22 10:20:16 +0200

    SHOPIFY-709 - Fix lint issues

--- extension/models/Errors/TokenRenewError.js
+++ extension/models/Errors/TokenRenewError.js
@@ -1,11 +1,10 @@
 const ETOKENRENEW = 'ETOKENRENEW'
-	
-	class TokenRenewError extends Error {
-	  constructor () {
-	    super('Error renewing customer access token.')
-	    this.code = ETOKENRENEW
-	  }
-	}
-	
-	module.exports = TokenRenewError
-  
+
+class TokenRenewError extends Error {
+  constructor () {
+    super('Error renewing customer access token.')
+    this.code = ETOKENRENEW
+  }
+}
+
+module.exports = TokenRenewError
