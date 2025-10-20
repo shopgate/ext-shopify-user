431122aecdc2c60668401c33d1708b189fa3d7ca
Timo Ebel <timo.ebel@shopgate.com>
2025-10-20 14:34:32 +0200

    CURB-5466 Implemented URL encryption and signing for Shopify Multipass if a Multipass token has been configured.

---
+++ extension/user/buildMultipassUrl.js
@@ -0,0 +1,22 @@
+const shopifyMultipass = require('../lib/shopifyMultipass')
+const { getBaseUrl } = require('../helper/config')
+
+/**
+ * @param {SDKContext} context
+ * @param {{ sgxsMeta: SgxsMeta, rawUrl: string }} input
+ */
+module.exports = async (context, input) => {
+  if (!context.meta.userId || !context.config.shopifyMultipassToken) return { url: input.rawUrl }
+
+  const baseUrl = getBaseUrl(context.config)
+  const { config: { shopifyMultipassToken } } = context
+  const { rawUrl } = input
+  const { user: { mail } } = await context.storage.user.get('userData')
+  const buyerIp = input.sgxsMeta.deviceIp
+
+  const url = shopifyMultipass(baseUrl, shopifyMultipassToken, mail, buyerIp, rawUrl)
+
+  console.log(url)
+
+  return { url }
+}
