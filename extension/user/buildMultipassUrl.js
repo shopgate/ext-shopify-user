7d4d5333796dc3b4b6496677da65759fb80ea706
Timo Ebel <timo.ebel@shopgate.com>
2025-10-21 01:28:09 +0200

    CURB-5466 Shopify Multipass: Implemented prepending the shop domain if the 'rawUrl' input contains a relative path; renamed 'rawUrl' pipeline input to 'target'.

--- extension/user/buildMultipassUrl.js
+++ extension/user/buildMultipassUrl.js
@@ -3,18 +3,21 @@ const { getBaseUrl } = require('../helper/config')
 
 /**
  * @param {SDKContext} context
- * @param {{ sgxsMeta: SgxsMeta, rawUrl: string }} input
+ * @param {{ sgxsMeta: SgxsMeta, target: string }} input
  */
 module.exports = async (context, input) => {
-  if (!context.meta.userId || !context.config.shopifyMultipassToken) return { url: input.rawUrl }
+  let target = input.target.match(/https?:\/\//)
+    ? input.target
+    : getBaseUrl(context.config) + input.target
+
+  if (!context.meta.userId || !context.config.shopifyMultipassToken) return { url: target }
 
   const baseUrl = getBaseUrl(context.config)
   const { config: { shopifyMultipassToken } } = context
-  const { rawUrl } = input
   const { user: { mail } } = await context.storage.user.get('userData')
   const buyerIp = input.sgxsMeta.deviceIp
 
-  const url = shopifyMultipass(baseUrl, shopifyMultipassToken, mail, buyerIp, rawUrl)
+  const url = shopifyMultipass(baseUrl, shopifyMultipassToken, mail, buyerIp, target)
 
   return { url }
 }
